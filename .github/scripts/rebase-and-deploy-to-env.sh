7bd479c02a0afa70eff6c8311b7356547e1f59af
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-05-02 14:41:29 +0200

    add script

---
+++ .github/scripts/rebase-and-deploy-to-env.sh
@@ -0,0 +1,22 @@
+# Env vars parameters: 
+# TARGET_BRANCH: dev, stable
+# SHA_COMMIT: the sha of the commit to rebase
+# AUTHOR: The user
+# GH_TOKEN: The token
+
+ORIGINAL__DIR=$PWD
+UPSTREAM_REPO_LOCATION=/tmp/upstream
+git clone https://github.com/$AUTHOR:$GH_TOKEN@github.com/5733d9e2be6485d52ffa08870cabdee0/sandbox.git $UPSTREAM_REPO_LOCATION > /dev/null 2>&1
+cd $UPSTREAM_REPO_LOCATION
+git checkout -b $TARGET_BRANCH > /dev/null 2>&1
+
+# If the commit is already on TARGET_BRANCH, the feature has been already merged and deployed.
+if [ $(git branch --contains $SHA_COMMIT | grep -c $TARGET_BRANCH) -ne 0 ]; then
+  echo $SHA_COMMIT is already on $TARGET_BRANCH branch!
+else
+  git rebase $SHA_COMMIT > /dev/null 2>&1
+  # git push origin $TARGET_BRANCH &> /dev/null
+  echo $SHA_COMMIT was not on $TARGET_BRANCH branch. $TARGET_BRANCH branch has been rebased and pushed to the upstream repository.
+fi
+
+rm -rf $UPSTREAM_REPO_LOCATION
