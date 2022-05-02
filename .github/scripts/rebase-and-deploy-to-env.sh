c01ed1e4ae41c311bf62c8b46ddb5c41eb981223
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-05-02 16:37:18 +0200

    Update rebase-and-deploy-to-env.sh

--- .github/scripts/rebase-and-deploy-to-env.sh
+++ .github/scripts/rebase-and-deploy-to-env.sh
@@ -12,17 +12,17 @@ if ! grep -q "$TARGET_BRANCH" <<< "${allowed_branch_values[@]}"; then
 fi
 
 UPSTREAM_REPO_LOCATION=/tmp/upstream
-git clone https://$AUTHOR:$GITHUB_TOKEN@github.com/5733d9e2be6485d52ffa08870cabdee0/sandbox.git $UPSTREAM_REPO_LOCATION # > /dev/null 2>&1
+git clone https://$AUTHOR:$GITHUB_TOKEN@github.com/5733d9e2be6485d52ffa08870cabdee0/sandbox.git $UPSTREAM_REPO_LOCATION > /dev/null 2>&1
 cd $UPSTREAM_REPO_LOCATION
 git checkout -b $TARGET_BRANCH > /dev/null 2>&1
 
 # If the commit is already on TARGET_BRANCH, the feature has been already merged and deployed.
 if [ $(git branch --contains $SHA_COMMIT | grep -c $TARGET_BRANCH) -ne 0 ]; then
-  echo $SHA_COMMIT is already on $TARGET_BRANCH branch!
+  echo "$SHA_COMMIT is already on $TARGET_BRANCH branch!"
 else
   git rebase $SHA_COMMIT > /dev/null 2>&1
   # git push origin $TARGET_BRANCH &> /dev/null
-  echo $SHA_COMMIT was not on $TARGET_BRANCH branch. $TARGET_BRANCH branch has been rebased and pushed to the upstream repository.
+  echo "$SHA_COMMIT was not on $TARGET_BRANCH branch. $TARGET_BRANCH branch has been rebased and pushed to the upstream repository."
 fi
 
 rm -rf $UPSTREAM_REPO_LOCATION
